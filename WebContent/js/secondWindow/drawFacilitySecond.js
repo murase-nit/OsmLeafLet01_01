fbae54f424315f40a6b1d5fb8c531299abb11ff5
Masaki Murase <murasemasaki_0216@yahoo.co.jp>
2015-06-17 16:09:32 +0900

    fix:マーカーの位置がずれていたので修正した

--- WebContent/js/secondWindow/drawFacilitySecond.js
+++ WebContent/js/secondWindow/drawFacilitySecond.js
@@ -65,7 +65,8 @@ DrawFacilitySecond.prototype.resizeFacility = function(aResizeX, aResizeY){
 //		console.info(g_drawSecondMap);
 //		console.info(g_drawFacilitySecond.facilityPoint[i].x);
 //		console.info(g_drawFacilitySecond.facilityPoint[i].y);
-			context.drawImage(img, g_drawFacilitySecond.facilityPoint[i].x*g_drawSecondMap.xScale, g_drawFacilitySecond.facilityPoint[i].y*g_drawSecondMap.yScale);
+			context.drawImage(img, g_drawFacilitySecond.facilityPoint[i].x*g_drawSecondMap.xScale-15,
+			 g_drawFacilitySecond.facilityPoint[i].y*g_drawSecondMap.yScale-41);
 	}
 };
 
